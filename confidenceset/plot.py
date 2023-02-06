406300f3d55f0a52bd8525d337bff116779f3a1b
HowonRyu <shryu94@g.ucla.edu>
2023-02-06 14:20:43 -0800

    added number of rejections

---
+++ confidenceset/plot.py
@@ -0,0 +1,48 @@
+def conf_plot(mu_set, noise_set, field_dim, threshold, alpha=0.05):
+  noise = get_noise(noise_set, np.array(field_dim))
+  mu = get_mu(mu_set, np.array(field_dim))
+  data = noise + mu
+  outer, inner, Achat, nrej = fdr_cope(data, threshold=threshold, alpha=alpha, tail="two")
+  plot = outer + inner + (mu[0,:,:]==threshold)
+  return(plot)
+
+
+
+def conf_plot_agg(c, _min=0, _max=3):
+  fig, axs = plt.subplots(2, 4, figsize=(30, 20))
+
+  im = axs[0, 0].imshow(conf_plot(mu_spec_circle_l_50, noise_spec_homogen, field_dim=(80,50,50),
+          threshold=c, alpha=0.05), vmin=_min, vmax=_max)
+  axs[0, 0].set_title("large circle (50*50)")
+
+
+  im = axs[0, 1].imshow(conf_plot(mu_spec_circle_s_50, noise_spec_homogen, field_dim=(80,50,50),
+          threshold=c, alpha=0.05), vmin=_min, vmax=_max)
+  axs[0, 1].set_title("small circle (50*50)")
+
+  im = axs[0, 2].imshow(conf_plot(mu_spec_circle_l_100, noise_spec_homogen, field_dim=(80,100,100),
+          threshold=c, alpha=0.05), vmin=_min, vmax=_max)
+  axs[0, 2].set_title("large circle (100*100)")
+
+  im = axs[0, 3].imshow(conf_plot(mu_spec_circle_s_100, noise_spec_homogen, field_dim=(80,100,100),
+          threshold=c, alpha=0.05), vmin=_min, vmax=_max)
+  axs[0, 3].set_title("small circle (100*100)")
+
+
+  im = axs[1, 0].imshow(conf_plot(mu_spec_circle_l_50, noise_spec_homogen_smth, field_dim=(80,50,50),
+          threshold=c,  alpha=0.05), vmin=_min, vmax=_max)
+  axs[1, 0].set_title("large circle (50*50, smoothed noise)")
+
+
+  im = axs[1, 1].imshow(conf_plot(mu_spec_circle_s_50, noise_spec_homogen_smth, field_dim=(80,50,50),
+          threshold=c, alpha=0.05),  vmin=_min, vmax=_max)
+  axs[1, 1].set_title("small circle (50*50, smoothed noise)")
+
+  im = axs[1, 2].imshow(conf_plot(mu_spec_circle_l_100, noise_spec_homogen_smth, field_dim=(80,100,100),
+          threshold=c, alpha=0.05),  vmin=_min, vmax=_max, )
+  axs[1, 2].set_title("large circle (100*100, smoothed noise)")
+
+
+  im = axs[1, 3].imshow(conf_plot(mu_spec_circle_s_100, noise_spec_homogen_smth, field_dim=(80,100,100),
+          threshold=c, alpha=0.05), vmin=_min, vmax=_max)
+  axs[1, 3].set_title("small circle (100*100, smoothed noise)")
---
+++ confidenceset/test.py
@@ -0,0 +1,36 @@
+def fdr_error_check(n_subj, img_dim, c, noise_set, mu_set,
+                    var=1, alpha=0.05, tail="two"):
+    data_dim = np.array((n_subj,) + img_dim)
+    noise = get_noise(noise_set, data_dim) * var
+    mu = get_mu(mu_set, data_dim)
+    data = mu + noise
+
+    lower, upper, Achat, all_sets, _ = fdr_cope(data, threshold=c, alpha=0.05, tail=tail)
+    Ac = mu >= c
+    AcC = 1 - Ac
+    upper_inclusion = 1 - np.any(AcC * upper > 0)
+    lower_inclusion = 1 - np.any(Ac * (1 - lower) > 0)
+    inclusion = upper_inclusion * lower_inclusion
+    exclusion = 1 - inclusion
+    return (exclusion)
+
+
+def fdr_error_check2(n_subj, img_dim, c, noise_set, mu_set,
+                     var=1, alpha=0.05, tail="two"):
+    data_dim = np.array((n_subj,) + img_dim)
+    noise = get_noise(noise_set, data_dim) * var
+    mu = get_mu(mu_set, data_dim)
+    data = mu + noise
+
+    lower, upper, Achat, all_sets, n_rej = fdr_cope(data, threshold=c, alpha=0.05, tail=tail)
+    Ac = mu >= c
+    AcC = 1 - Ac
+
+    numer = np.sum(np.minimum(np.maximum(upper - Ac.astype(int), 0) + np.maximum(Ac.astype(int) - lower, 0), 1))
+    denom = np.sum(np.minimum(upper + (1 - lower), 1))
+
+    if n_rej == 0:
+        ERR = 0
+    else:
+        ERR = numer / denom
+    return (ERR)
